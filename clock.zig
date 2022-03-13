615077f89aeaf1307b56b31770b5ed18ff0e02dc
Steven Schmid <stevschmid@gmail.com>
2022-03-13 13:11:17 +0100

    Clock

---
+++ clock.zig
@@ -0,0 +1,60 @@
+const std = @import("std");
+const vector = @import("vector.zig");
+const Vec4 = vector.Vec4;
+const Mat4 = @import("matrix.zig").Mat4;
+const Canvas = @import("canvas.zig").Canvas;
+const Color = @import("color.zig").Color;
+const canvasToPPM = @import("ppm.zig").canvasToPPM;
+
+const Projectile = struct {
+    pos: Vec4,
+    velocity: Vec4,
+};
+
+const Environment = struct {
+    gravity: Vec4,
+    wind: Vec4,
+};
+
+fn tick(proj: Projectile, env: Environment) Projectile {
+    return .{
+        .pos = proj.pos.add(proj.velocity),
+        .velocity = proj.velocity.add(env.gravity).add(env.wind),
+    };
+}
+
+fn addPointToCanvas(canvas: *Canvas, point: Vec4) void {
+    const w = @intToFloat(f32, canvas.width);
+    const h = @intToFloat(f32, canvas.height);
+
+    // 0, 0 is middle, 0.5, 0.5 is edge
+    const x = 0.5 + point.x;
+    const y = 0.5 + point.y;
+
+    canvas.set(@floatToInt(usize, x * w), @floatToInt(usize, (1.0 - y) * h), Color.init(1, 1, 1));
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var canvas = try Canvas.init(allocator, 100, 100);
+    defer canvas.deinit();
+
+    var cursor = vector.initVector(0, 0.3, 0);
+
+    var rot = Mat4.identity().rotateZ(-2.0 * std.math.pi / 12.0);
+
+    var i: usize = 0;
+    while (i < 12) : (i += 1) {
+        addPointToCanvas(&canvas, cursor);
+        cursor = rot.multVec(cursor);
+    }
+
+    const dir: std.fs.Dir = std.fs.cwd();
+    const file: std.fs.File = try dir.createFile("/tmp/result.ppm", .{});
+    defer file.close();
+
+    try canvasToPPM(canvas, file.writer());
+}
