ee49ca3e20b0efe06c14696530a2f283354a7569
Steven Schmid <stevschmid@gmail.com>
2022-03-13 10:16:17 +0100

    More matrix fn

--- matrix.zig
+++ matrix.zig
@@ -1,61 +1,87 @@
 const std = @import("std");
 const utils = @import("utils.zig");
 const epsilonEq = @import("utils.zig").epsilonEq;
-const Tuple = @import("tuple.zig").Tuple;
+const Vec4 = @import("vector.zig").Vec4;
 
-pub fn Matrix(comptime Size: usize) type {
-    return struct {
-        const Self = @This();
+pub const Mat4 = struct {
+    const Self = @This();
 
-        pub const num_rows = Size;
-        pub const num_cols = Size;
+    pub const num_rows = 4;
+    pub const num_cols = 4;
 
-        data: [num_rows][num_cols]f32 = undefined,
+    mat: [num_rows][num_cols]f32 = undefined,
 
-        pub fn eql(self: Self, other: Self) bool {
-            for (self.data) |row, row_idx| {
-                for (row) |val, col_idx| {
-                    if (!epsilonEq(val, other.data[row_idx][col_idx]))
-                        return false;
-                }
+    pub fn identity() Self {
+        return .{
+            .mat = .{
+                .{ 1, 0, 0, 0 },
+                .{ 0, 1, 0, 0 },
+                .{ 0, 0, 1, 0 },
+                .{ 0, 0, 0, 1 },
+            },
+        };
+    }
+
+    pub fn eql(self: Self, other: Self) bool {
+        for (self.mat) |row_values, row| {
+            for (row_values) |val, col| {
+                if (!epsilonEq(val, other.mat[row][col]))
+                    return false;
             }
-
-            return true;
         }
 
-        pub fn mult(self: Self, other: Self) Self {
-            const a = self.data;
-            const b = other.data;
+        return true;
+    }
 
-            var res = Self{};
+    pub fn mult(self: Self, other: Self) Self {
+        const a = self.mat;
+        const b = other.mat;
 
-            for (res.data) |*row, row_idx| {
-                for (row) |*val, col_idx| {
-                    val.* = 0.0;
+        var res = Self{};
 
-                    var i: usize = 0;
-                    while (i < Size) : (i += 1) {
-                        val.* += a[row_idx][i] * b[i][col_idx];
-                    }
-                }
+        for (res.mat) |*row_values, row| {
+            for (row_values) |*val, col| {
+                val.* = a[row][0] * b[0][col] +
+                    a[row][1] * b[1][col] +
+                    a[row][2] * b[2][col] +
+                    a[row][3] * b[3][col];
             }
-
-            return res;
         }
 
-        pub fn at(self: Self, row: usize, col: usize) f32 {
-            return self.data[row][col];
-        }
-    };
-}
+        return res;
+    }
 
-pub const Mat2 = Matrix(2);
-pub const Mat3 = Matrix(3);
-pub const Mat4 = Matrix(4);
+    pub fn multVec(self: Self, vec: Vec4) Vec4 {
+        const m = self.mat;
+
+        return Vec4.init(
+            m[0][0] * vec.x + m[0][1] * vec.y + m[0][2] * vec.z + m[0][3] * vec.w,
+            m[1][0] * vec.x + m[1][1] * vec.y + m[1][2] * vec.z + m[1][3] * vec.w,
+            m[2][0] * vec.x + m[2][1] * vec.y + m[2][2] * vec.z + m[2][3] * vec.w,
+            m[3][0] * vec.x + m[3][1] * vec.y + m[3][2] * vec.z + m[3][3] * vec.w,
+        );
+    }
+
+    pub fn at(self: Self, row: usize, col: usize) f32 {
+        return self.mat[row][col];
+    }
+
+    pub fn transpose(self: Self) Self {
+        var res = Self{};
+
+        for (res.mat) |*row_values, row| {
+            for (row_values) |*val, col| {
+                val.* = self.mat[col][row];
+            }
+        }
+
+        return res;
+    }
+};
 
 test "constructing and inspecting a 4x4 matrix" {
     const m = Mat4{
-        .data = .{
+        .mat = .{
             .{ 1, 2, 3, 4 },
             .{ 5.5, 6.5, 7.5, 8.5 },
             .{ 9, 10, 11, 12 },
@@ -72,37 +98,9 @@ test "constructing and inspecting a 4x4 matrix" {
     try utils.expectEpsilonEq(m.at(3, 2), 15.5);
 }
 
-test "a 2x2 matrix ought to be representable" {
-    const m = Mat2{
-        .data = .{
-            .{ -3, 5 },
-            .{ 1, -2 },
-        },
-    };
-
-    try utils.expectEpsilonEq(m.at(0, 0), -3);
-    try utils.expectEpsilonEq(m.at(0, 1), 5);
-    try utils.expectEpsilonEq(m.at(1, 0), 1);
-    try utils.expectEpsilonEq(m.at(1, 1), -2);
-}
-
-test "a 3x3 matrix ought to be representable" {
-    const m = Mat3{
-        .data = .{
-            .{ -3, 5, 0 },
-            .{ 1, -2, -7 },
-            .{ 0, 1, 1 },
-        },
-    };
-
-    try utils.expectEpsilonEq(m.at(0, 0), -3);
-    try utils.expectEpsilonEq(m.at(1, 1), -2);
-    try utils.expectEpsilonEq(m.at(2, 2), 1);
-}
-
 test "matrix equality with identical matrices" {
     const a = Mat4{
-        .data = .{
+        .mat = .{
             .{ 1, 2, 3, 4 },
             .{ 5, 6, 7, 8 },
             .{ 9, 8, 7, 6 },
@@ -111,7 +109,7 @@ test "matrix equality with identical matrices" {
     };
 
     const b = Mat4{
-        .data = .{
+        .mat = .{
             .{ 1, 2, 3, 4 },
             .{ 5, 6, 7, 8 },
             .{ 9, 8, 7, 6 },
@@ -124,7 +122,7 @@ test "matrix equality with identical matrices" {
 
 test "matrix equality with different matrices" {
     const a = Mat4{
-        .data = .{
+        .mat = .{
             .{ 1, 2, 3, 4 },
             .{ 5, 6, 7, 8 },
             .{ 9, 8, 7, 6 },
@@ -133,7 +131,7 @@ test "matrix equality with different matrices" {
     };
 
     const b = Mat4{
-        .data = .{
+        .mat = .{
             .{ 1, 2, 3, 4 },
             .{ 5, 6, 7, 8 },
             .{ 9, 8, 7, 5 },
@@ -146,7 +144,7 @@ test "matrix equality with different matrices" {
 
 test "multiplying two matrices" {
     const a = Mat4{
-        .data = .{
+        .mat = .{
             .{ 1, 2, 3, 4 },
             .{ 5, 6, 7, 8 },
             .{ 9, 8, 7, 6 },
@@ -155,7 +153,7 @@ test "multiplying two matrices" {
     };
 
     const b = Mat4{
-        .data = .{
+        .mat = .{
             .{ -2, 1, 2, 3 },
             .{ 3, 2, 1, -1 },
             .{ 4, 3, 6, 5 },
@@ -166,7 +164,7 @@ test "multiplying two matrices" {
     const result = a.mult(b);
 
     const expected = Mat4{
-        .data = .{
+        .mat = .{
             .{ 20, 22, 50, 48 },
             .{ 44, 54, 114, 108 },
             .{ 40, 58, 110, 102 },
@@ -175,4 +173,58 @@ test "multiplying two matrices" {
     };
 
     try std.testing.expect(result.eql(expected));
+}
+
+test "a matrix multiplied by a tuple" {
+    const a = Mat4{
+        .mat = .{
+            .{ 1, 2, 3, 4 },
+            .{ 2, 4, 4, 2 },
+            .{ 8, 6, 4, 1 },
+            .{ 0, 0, 0, 1 },
+        },
+    };
+
+    const b = Vec4.init(1, 2, 3, 1);
+
+    const result = a.multVec(b);
+
+    try std.testing.expect(result.eql(Vec4.init(18, 24, 33, 1)));
+}
+
+test "multiplying a matrix by the identity matrix" {
+    const a = Mat4{
+        .mat = .{
+            .{ 0, 1, 2, 4 },
+            .{ 1, 2, 4, 8 },
+            .{ 2, 4, 8, 16 },
+            .{ 4, 8, 16, 32 },
+        },
+    };
+
+    const identity_matrix = Mat4.identity();
+
+    try std.testing.expect(a.mult(identity_matrix).eql(a));
+}
+
+test "transposing a matrix" {
+    const a = Mat4{
+        .mat = .{
+            .{ 0, 9, 3, 0 },
+            .{ 9, 8, 0, 8 },
+            .{ 1, 8, 5, 3 },
+            .{ 0, 0, 5, 8 },
+        },
+    };
+
+    const expected = Mat4{
+        .mat = .{
+            .{ 0, 9, 1, 0 },
+            .{ 9, 8, 8, 0 },
+            .{ 3, 0, 5, 5 },
+            .{ 0, 8, 3, 8 },
+        },
+    };
+
+    try std.testing.expect(a.transpose().eql(expected));
 }
